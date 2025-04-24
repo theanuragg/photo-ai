b36becbb8112380db0bb6c99bc1dee5fbe3ec49b
Anurag Singh <anurags9693@gmail.com>
2025-04-25 00:31:07 +0500

    Refactor authMiddleware to use Clerk for user authentication and update package dependencies

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -1,25 +1,48 @@
 import type { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { prismaClient } from "db";
+import { clerkClient } from "@clerk/clerk-sdk-node";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader?.split(" ")[1];
-
-    try {
-        const decoded = jwt.decode(token, process.env.AUTH_JWT_KEY, {
-            algorithms: ['RS256'] 
-        })
-        if (decoded?.sub) {
-            req.userId = decoded?.sub;
-            next();
-        } else {
-            res.status(403).json({
-                message: "Error while decoding"
-            })
-        }
-    } catch(e) {
-        res.status(403).json({
-            message: "Error while decoding"
-        })
+export async function  authMiddleware(req: Request, res: Response, next: NextFunction) {
+  
+    const clerkId = req.headers.clerkid as string | undefined;
+    if (!clerkId) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
     }
+
+    
+    let email = "";
+    try {
+      const clerkUser = await clerkClient.users.getUser(clerkId);
+      email = clerkUser.emailAddresses[0]?.emailAddress || "";
+    } catch (err) {
+      console.error("Error fetching user from Clerk:", err);
+      res.status(401).json({ error: "Failed to verify Clerk user" });
+      return;
+    }
+  
+    if (!email) {
+      res.status(401).json({ error: "No email found for user" });
+      return;
+    }
+  
+    let user = await prismaClient.user.findUnique({
+      where: {
+        clerk: clerkId,
+      },
+    });
+  
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          clerk: clerkId,
+          email: email,
+          username: email.split("@")[0], // Use the part before "@" as the username
+        },
+      });
+    }
+    req.userId = user.id;
+    next();
+   
+
 }
