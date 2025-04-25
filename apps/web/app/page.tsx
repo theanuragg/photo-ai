3203e3c6801df1a210a467f59db944bb85a836bb
Anurag Singh <anurags9693@gmail.com>
2025-04-25 13:07:29 +0500

    Remove unused imports from page.tsx to clean up the code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@/components/ui/button";
+'use client";'
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
